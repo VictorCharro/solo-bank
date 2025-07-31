9ae2650dd4bdb069e0552e3ca8d1cdf1ebfa18ae
VcsDevs <v_chs@hotmail.com>
2025-07-31 15:32:40 -0300

    Implementando findBy nos repositories

--- src/main/java/com/solo_bank/Solo_Bank/repositories/ClienteRepository.java
+++ src/main/java/com/solo_bank/Solo_Bank/repositories/ClienteRepository.java
@@ -4,6 +4,11 @@ import com.solo_bank.Solo_Bank.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository <Cliente, Long> {
+
+    public Optional<Cliente> findById(Long id);
+    public Optional<Cliente> findByCpf(Long cpf);
 }
--- src/main/java/com/solo_bank/Solo_Bank/repositories/ContaRepository.java
+++ src/main/java/com/solo_bank/Solo_Bank/repositories/ContaRepository.java
@@ -4,6 +4,10 @@ import com.solo_bank.Solo_Bank.entities.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContaRepository extends JpaRepository <Conta, Long> {
+
+    public Optional<Conta> findByNumeroDaConta(int numeroDaConta);
 }
