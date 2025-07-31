fcde207618159725a3cf971b13f93adb7f1c9da0
VcsDevs <v_chs@hotmail.com>
2025-07-31 15:09:28 -0300

    First commit

---
+++ src/main/java/com/solo_bank/Solo_Bank/SoloBankApplication.java
@@ -0,0 +1,13 @@
+package com.solo_bank.Solo_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solo_bank/Solo_Bank/SoloBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo_bank.Solo_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
